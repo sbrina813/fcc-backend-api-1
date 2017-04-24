dd3904588598c4fd63e97e94ee319f3ff8685c19
Sbrina813 <sabrina.jiang813@gmail.com>
2017-04-24 23:03:52 +0000

    Made code changes

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var port = process.env.PORT || 8080;
+var moment = require('moment');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-})
+app.get('/:timestamp', function(req,res) {
+  var date;
+  if(!isNaN(parseInt(req.params.timestamp,10))) {
+    date = moment(req.params.timestamp, "X");
+  } else {
+    date = moment(req.params.timestamp, "MMMM D, YYYY");
+  }
+
+  if(date.isValid()) {
+    res.json({
+      unix: date.format("X"),
+      natural: date.format("MMMM D, YYYY")
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
+
+
+});
